30404fee6d3d066a7b7fc2ffceeaa1c33c01d96c
Maximo Piva <maximo.piva@gmail.com>
2016-07-19 00:37:35 -0300

    Added IAppAuthorization

---
+++ NutzCode.CloudFileSystem/IAppAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutzCode.CloudFileSystem
+{
+    public interface IAppAuthorization
+    {
+        Dictionary<string, object> Get(string provider);
+    }
+}
